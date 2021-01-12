064ea222cc89ecb8087b928921b64ee5c12843ca
PolinaRrr <poniapocalypsisa@gmail.com>
2021-01-12 17:03:19 +0300

    Commit#3 Добавить в проект элемент CalendarView.

--- app/src/main/java/com/example/hw_1_1_05_01_2021/MainActivity.java
+++ app/src/main/java/com/example/hw_1_1_05_01_2021/MainActivity.java
@@ -1,31 +1,32 @@
 package com.example.hw_1_1_05_01_2021;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+    Button button_next_page1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_next_page1 = findViewById(R.id.button_next_page1);
 
-        Button button_next_page = findViewById(R.id.button_next);
-        button_next_page.setOnClickListener(new View.OnClickListener() {
+
+        button_next_page1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.new_layout);
+                Intent page_second = new Intent(MainActivity.this, PageSecond.class);
+                startActivity(page_second);
             }
         });
 
     }
-
-
-
 }
+
