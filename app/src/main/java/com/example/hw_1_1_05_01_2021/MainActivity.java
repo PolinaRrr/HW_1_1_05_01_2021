2bd6ff8cba184a3900b1a711d0daed8592f560e1
PolinaRrr <poniapocalypsisa@gmail.com>
2021-01-12 14:07:21 +0300

    Commit#2 Создать ещё один макет (если не получается, то проект) с несколькими EditText, где использовать атрибут inputType: text, textPersonName, phone, number, textPassword, textEmailAddress и другие значения

--- app/src/main/java/com/example/hw_1_1_05_01_2021/MainActivity.java
+++ app/src/main/java/com/example/hw_1_1_05_01_2021/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.hw_1_1_05_01_2021;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_next_page = findViewById(R.id.button_next);
+        button_next_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.new_layout);
+            }
+        });
+
     }
+
+
+
 }
